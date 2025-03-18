9d855976d1719e0e620d79e3723f931acfbbbb2e
Nguyễn Minh Đức <108139083+minhduc1212@users.noreply.github.com>
2025-03-19 00:50:22 +0700

    Update ex3.c

--- Labw_5/ex3.c
+++ Labw_5/ex3.c
@@ -47,13 +47,41 @@ int insertion(int n[]){
     }
 }
 
+int selection (int n []) {
+    printf("Selection sort\n");
+    printf("Before ");
+    for (int i = 0; i < 7; i++) {
+        printf("%d ", n[i]);
+    }
+    printf("\n");
+
+    for(int i = 0; i < 7; i++) {
+        int min = i;
+        for (int j = i+1; j < 7; j++) {
+            if (n[j] < n[min]) {
+                int temp = n[j];
+                n[j] = n[min];
+                n[min] = temp;
+            }
+        }
+    }
+
+    printf("After ");
+    for (int i = 0; i < 7; i++) {
+        printf("%d ", n[i]);
+    }
+}
+
 int main () {
     int n1[7] = {2, 8, 5, 3, 9, 4, 1};
     int n2[7] = {2, 8, 5, 3, 9, 4, 1};
+    int n3[7] = {2, 8, 5, 3, 9, 4, 1};
     
     bubble(n1);
     printf("\n");
     insertion(n2);
+    printf("\n");
+    selection(n3);
     
     return 0;
 }
